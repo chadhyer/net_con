620619c3fcb70001301f2a5e53f2f228bec74e56
chadhyer <chadhyer92@gmail.com>
2022-06-26 16:52:21 +0100

    setup device_create and device_lookup API endpoints

--- app.py
+++ app.py
@@ -1,41 +1,58 @@
+import re
 from settings import *
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, render_template, request
+from db import insert_device, select_device
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello World!</p>"
+    return "<p>Hello World!</p>"
+
+
+@app.route(f"/device_create/", methods=['GET'])
+def device_create():
+    return render_template('deivce_create.html')
 
 
 @app.route(f"/device_lookup/", methods=['GET'])
 def device_lookup():
-  return render_template('device_lookup.html')
+    return render_template('device_lookup.html')
 
 
-@app.route("/device/", methods=['GET'])
+@app.route("/device/", methods=['GET', 'POST', 'PUT'])
 def device():
-  if request.method == 'POST':
-    if request.form['submit'] == 'lookup':
-      print('lookup')
-    if request.form['submit'] == 'save':
-      print('save')
-    print('post')
-  if request.method == 'GET':
-    print('get')
+    if request.method == 'POST':
+        label = request.form.get('label', None)
+        location = request.form.get('location', None)
+        serial = request.form.get('serial', None)
+        ip = request.form.get('ip', None)
+        mac = request.form.get('mac', None)
+        insert_device(label=label, 
+                      location=location,
+                      serial=serial,
+                      ip=ip,
+                      mac=mac)
+        return "<p>Device added</p>"
+
+    if request.method == 'GET':
+        print('get')
+        device_list = select_device()
+        return 
 
 
 @app.route(f"/device/<dev_id>", methods=['GET'])
 def get_device(dev_id):
-  content = request.get_json
-  print(content)
-  return dev_id
+    content = request.get_json
+    print(content)
+    return dev_id
 
 
 @app.route(f"/device/<dev_id>/status/", methods=['GET'])
 def get_device_health(dev_id):
-  pass
+    pass
+
 
 if __name__ == '__main__':
-  app.run(debug = True)
+    app.run(debug = True)
